dc2b8f113fcae8f02b7ee711d35b1d02da54c75d
westmount <lisong@dajiazhongyi.com>
2018-05-16 19:50:58 +0800

    chapter 6

---
+++ src/chap6/P1a.java
@@ -0,0 +1,12 @@
+package chap6;
+
+import static chap6.p1.P1.test;
+
+/**
+ * Created by Song on 2018/5/16.
+ */
+public class P1a {
+  public static void main(String[] args) {
+    test();
+  }
+}
---
+++ src/chap6/p1/P1.java
@@ -0,0 +1,10 @@
+package chap6.p1;
+
+/**
+ * Created by Song on 2018/5/16.
+ */
+public class P1 {
+  public static void test() {
+    System.out.println("test");
+  }
+}
