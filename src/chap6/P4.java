378748a8d9bce2a344abaa34262caeabb97e0c68
westmount <lisong@dajiazhongyi.com>
2018-05-17 15:50:26 +0800

    p4

---
+++ src/chap6/P4.java
@@ -0,0 +1,12 @@
+package chap6;
+
+/**
+ * Created by Song on 2018/5/17.
+ */
+
+public class P4 {
+  public static void main(String[] args) {
+    P4_Test test = new P4_Test();
+    test.f();
+  }
+}
---
+++ src/chap6/P4_Test.java
@@ -0,0 +1,17 @@
+package chap6;
+
+/**
+ * Created by Song on 2018/5/17.
+ */
+public class P4_Test {
+
+  protected void f() {
+    System.out.println("f");
+  }
+
+  private void g() {
+    System.out.println("g");
+  }
+
+
+}
